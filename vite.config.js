8c14ff4897960dc1ce8d72cbd1371e396870a978
Muhammad Anas <134198144+Muhammad-Anas-3@users.noreply.github.com>
2023-12-07 09:39:27 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/image-gallery/', // Replace 'image-gallery' with your GitHub repository name
+  base: '/image-gallery/',
   plugins: [react()],
 });
